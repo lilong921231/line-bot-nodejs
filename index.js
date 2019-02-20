b466c09958f7d45b676f225b3797bf3dad546fbc
lilong <568639606@qq.com>
2019-02-20 12:34:56 +0800

    自动发送 bot

--- index.js
+++ index.js
@@ -44,6 +44,44 @@ function ceshi(event) {
     var ceshi = event.message.text;
     if(ceshi === '1') {
         return ceshi;
+    } else if (ceshi === '2'){
+        var button = {
+            type: "template",
+            altText: "This is a buttons template",
+            template: {
+                type: "buttons",
+                thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
+                imageAspectRatio: "rectangle",
+                imageSize: "cover",
+                imageBackgroundColor: "#FFFFFF",
+                title: "Menu",
+                text: "Please select",
+                defaultAction: {
+                    type: "uri",
+                    label: "View detail",
+                    uri: "http://example.com/page/123"
+                },
+                actions: [
+                    {
+                        type: "postback",
+                        label: "Buy",
+                        data: "action=buy&itemid=123"
+                    },
+                    {
+                        type: "postback",
+                        label: "Add to cart",
+                        data: "action=add&itemid=123"
+                    },
+                    {
+                        type: "uri",
+                        label: "View detail",
+                        uri: "http://example.com/page/123"
+                    }
+                ]
+            }
+        };
+
+        return button;
     } else {
         var baidu = 'http://wwww.baidu.com';
 
@@ -51,6 +89,9 @@ function ceshi(event) {
 
         return tishi + time5(baidu);
     }
+
+
+
 }
 
 function time5(url) {
