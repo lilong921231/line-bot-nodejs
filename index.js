d530b75237069ebb9dbed26554d666043370c3d2
lilong <568639606@qq.com>
2019-02-20 13:20:45 +0800

    自动发送 bot

--- index.js
+++ index.js
@@ -48,43 +48,7 @@ function ceshi(event) {
         var button = {
             type: "template",
             altText: "This is a buttons template",
-            template: [{
-                type: "buttons",
-                thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
-                imageAspectRatio: "rectangle",
-                imageSize: "cover",
-                imageBackgroundColor: "#FFFFFF",
-                title: "Menu",
-                text: "Please select",
-                defaultAction: [{
-                    type: "uri",
-                    label: "View detail",
-                    uri: "http://example.com/page/123"
-                },
-                 {
-                    type: "uri",
-                    label: "View detail",
-                    uri: "http://example.com/page/123"
-                }],
-                actions: [
-                    {
-                        type: "postback",
-                        label: "Buy",
-                        data: "action=buy&itemid=123"
-                    },
-                    {
-                        type: "postback",
-                        label: "Add to cart",
-                        data: "action=add&itemid=123"
-                    },
-                    {
-                        type: "uri",
-                        label: "View detail",
-                        uri: "http://example.com/page/123"
-                    }
-                ]
-
-            },
+            template: [
                 {
                     type: "buttons",
                     thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
@@ -93,11 +57,51 @@ function ceshi(event) {
                     imageBackgroundColor: "#FFFFFF",
                     title: "Menu",
                     text: "Please select",
-                    defaultAction: [{
-                        type: "uri",
-                        label: "View detail",
-                        uri: "http://example.com/page/123"
-                    },
+                    defaultAction: [
+                        {
+                            type: "uri",
+                            label: "View detail",
+                            uri: "http://example.com/page/123"
+                        },
+                        {
+                            type: "uri",
+                            label: "View detail",
+                            uri: "http://example.com/page/123"
+                        }
+                    ],
+                    actions: [
+                        {
+                            type: "postback",
+                            label: "Buy",
+                            data: "action=buy&itemid=123"
+                        },
+                        {
+                            type: "postback",
+                            label: "Add to cart",
+                            data: "action=add&itemid=123"
+                        },
+                        {
+                            type: "uri",
+                            label: "View detail",
+                            uri: "http://example.com/page/123"
+                        }
+                    ]
+
+                },
+                {
+                    type: "buttons",
+                    thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
+                    imageAspectRatio: "rectangle",
+                    imageSize: "cover",
+                    imageBackgroundColor: "#FFFFFF",
+                    title: "Menu",
+                    text: "Please select",
+                    defaultAction: [
+                        {
+                            type: "uri",
+                            label: "View detail",
+                            uri: "http://example.com/page/123"
+                        },
                         {
                             type: "uri",
                             label: "View detail",
