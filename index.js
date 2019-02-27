d1b9012bb163776cde59ac60c6805e0665fcffa8
lilong <568639606@qq.com>
2019-02-27 08:41:23 +0800

    自动发送 bot

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var linebot = require('linebot');
 var express = require('express');
-var bot = require('./line.config');
-/*var bot = linebot({
+// var bot = require('./line.config');
+var bot = linebot({
     channelId: "1646638453",
     channelSecret: "c20aee7e28b0b762cbf5adb9e8a1fa96",
     channelAccessToken: "noRmAnxXUXdfOEmrasRRY0IF/YPJd+6WF5XXtQ0Nzhl438A+cvFalWwMAOSY1V2hwY3e6xQERfmGBhv2CxuCiJDF80xy4Ryo9N/" +
         "mMTsd+6z5AZNXJI6fXtL2eCr/gUSIovXzKIDaGBlEDFSaF7BW4gdB04t89/1O/w1cDnyilFU="
-});*/
+});
 
 function ceshi(event) {
     var ceshi = event.message.text;
