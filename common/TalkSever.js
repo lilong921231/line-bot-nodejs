8b873227051b362ce8b50a7729d90067bed28939
lilong <568639606@qq.com>
2019-02-27 16:21:19 +0800

    自动发送 bot

--- common/TalkSever.js
+++ common/TalkSever.js
@@ -15,7 +15,7 @@ function talkServer(eventSelect) {
         case '3':
             return {
                 type: "image",
-                originalContentUrl: "../..//public/images/wages.png",
+                originalContentUrl: "../../public/images/wages.png",
                 previewImageUrl: "https://github.com/lilong921231/line-bot-nodejs/blob/master/public/images/wages.png"
             };
     }
