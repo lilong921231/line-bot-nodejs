9dc4319eb7ee68475f39befae1e31aeb0a531198
lilong921231 <46079570+lilong921231@users.noreply.github.com>
2019-02-27 09:55:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // var linebot = require('linebot');
 var express = require('express');
-var bot = require('line.config');
+var bot = require('./line.config');
 
 // var bot = linebot({
 //     channelId: "1646638453",
