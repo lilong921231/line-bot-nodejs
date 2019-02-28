0cebcdd2f4f8e23cf4a02de2f826d56699ca7710
lilong <568639606@qq.com>
2019-02-28 15:55:13 +0800

    自动发送 bot

--- common/TalkSever.js
+++ common/TalkSever.js
@@ -39,9 +39,10 @@ function talkServer(eventSelect) {
                             {
                                 type: "button",
                                 action: {
-                                    type: "message",
+                                    type: "postback",
                                     label: "OK",
                                     text: "2",
+                                    data: "action=buy&itemid=111"
                                     // contents: talk.detailedOfWeek('2018-11-09', day, '500000', '-2000000', '20000000')
                                 }
                             }
