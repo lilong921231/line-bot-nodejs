4bc47675d3200ec7ae9046a66ad5acf23f32b249
lilong <568639606@qq.com>
2019-02-28 13:28:27 +0800

    自动发送 bot

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ server.post('/', line.middleware(line_config), (req, res, next) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         // if (event.type === "message" && event.message.type === "text"){
             events_processed.push(bot.replyMessage(event.replyToken,
-                serverTest.talkServer(event.message.data)));
+                serverTest.talkServer(event.message.text)));
         // }
     });
 
