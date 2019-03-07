e8aec35a79f2b584ae0c4df185adcf9af7fc403d
lilong <568639606@qq.com>
2019-03-07 16:23:10 +0800

    sever

--- common/sever/MondayTalkSever.js
+++ common/sever/MondayTalkSever.js
@@ -44,7 +44,7 @@ function Monday(event) {
 }
 
 function dataAccess_Monday(eMail) {
-    dataAccess.Monday(eMali).then(myData => {
+    dataAccess.Monday(eMail).then(myData => {
         console.log(data);
         return myData;
     }).catch(err => {
